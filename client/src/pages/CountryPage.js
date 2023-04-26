5b0370c1ab2dc99519f9e3d29a72f7d71b74997a
abhishekbhor <abh.bhor@gmail.com>
2023-04-26 17:17:38 -0400

    Country Page

--- client/src/pages/CountryPage.js
+++ client/src/pages/CountryPage.js
@@ -1,36 +1,88 @@
 import { useEffect, useState } from 'react';
-import { Container, Divider, Link } from '@mui/material';
+import { Container} from '@mui/material';
 import { NavLink } from 'react-router-dom';
-
+import * as React from 'react';
 import LazyTable from '../components/LazyTable';
-import SongCard from '../components/SongCard';
 const config = require('../config.json');
 
-export default function CountryPage() {
-    // We use the setState hook to persist information across renders (such as the result of our API calls)
-    const [randomIndicator, setRandomIndicator] = useState({});
-    // TODO (TASK 13): add a state variable to store the app author (default to '')
-    const [averages, setAverages] = useState({});
-    const [countries, setCountry] = useState(null);
+//let urlInd = encodeURIComponent()
 
-    // The useEffect hook by default runs the provided callback after every render
-    // The second (optional) argument, [], is the dependency array which signals
-    // to the hook to only run the provided callback if the value of the dependency array
-    // changes from the previous render. In this case, an empty array means the callback
-    // will only run on the very first render.
+export default function CountriesPage() {
+    // We use the setState hook to persist information across renders (such as the result of our API calls)
+
+    const [randomIndicatorCat, setRandomIndicatorCat] = useState({
+        "data": [
+            {
+                "category": "Jobs"
+            },
+        ]
+    });
+
+    const [randomIndicator, setRandomIndicator] = useState({
+        "data": [
+            {
+                "category": "Tax payments (number)"
+            },
+        ]
+    });
+
+    const [countries, setCountries] = useState({
+        "data": [
+            {
+                "category": "Aruba"
+            },
+        ]
+    });
+
+    const [valueIndCode, setValueIndCode] = React.useState('IC_TAX_PAYM');
+
+    const [valueCat, setValueCat] = React.useState('Jobs');
+    const [valueInd, setValueInd] = React.useState('Tax payments (number)');
+
+    const [valueCountries, setValueCountries] = React.useState('Aruba');
+
+    const handleChangeCat = (event) => {
+        setValueCat(event.target.value);
+    }
+    const handleChangeInd = (event) => {
+        setValueInd(event.target.value);
+
+    }
+    const handleChangeCountries = (event) => {
+        setValueCountries(event.target.value);
+    }
+
+
     useEffect(() => {
-        // Fetch request to get the song of the day. Fetch runs asynchronously.
-        // The .then() method is called when the fetch request is complete
-        // and proceeds to convert the result to a JSON which is finally placed in state.
-        fetch(`http://${config.server_host}:${config.server_port}/random`)
+
+        fetch(`http://${config.server_host}:${config.server_port}/randomIndCat`)
+            .then(res => res.json())
+            .then(resJson => setRandomIndicatorCat(resJson));
+
+        fetch(`http://${config.server_host}:${config.server_port}/indicatorsOnCat/${valueCat}`)
             .then(res => res.json())
             .then(resJson => setRandomIndicator(resJson));
 
-        // TODO (TASK 14): add a fetch call to get the app author (name not pennkey) and store it in the state variable
-        fetch(`http://${config.server_host}:${config.server_port}/top5/:indicator`)
+        fetch(`http://${config.server_host}:${config.server_port}/countries`)
             .then(res => res.json())
-            .then(resJson => setAverages(resJson));
-    }, []);
+            .then(resJson => setCountries(resJson));
+
+            fetch(`http://${config.server_host}:${config.server_port}/indName2indCode/${urlInd}`)
+            .then(res => res.json())
+            .then(resJson => setValueIndCode(resJson));
+
+
+
+    }, [valueCat, valueInd, valueCountries, valueIndCode]);
+
+
+    let urlInd = encodeURIComponent(valueInd)
+
+    console.log(valueCat)
+    console.log(valueInd)
+    console.log(valueCountries)
+    console.log(valueIndCode)
+    console.log(urlInd)
 
 
     // TODO (TASK 15): define the columns for the top albums (schema is Album Title, Plays), where Album Title is a link to the album page
@@ -94,27 +146,55 @@ export default function CountryPage() {
             <h1>This is the Country page</h1>
             <div>
                 <label>
-                    Select Indicator Category
-                    <select>
-                        <option value="randomIndicator">{setRandomIndicator}</option>
-                    </select>
+                    Select Indicator Category:
                 </label>
+                <p></p>
+                <select value={valueCat} onChange={handleChangeCat}>
+                    {randomIndicatorCat.data.map((option) => (
+                        <option key={option.category} value={option.category}>
+                            {option.category}
+                        </option>
+                    ))}
+                </select>
+                <p></p>
                 <label>
-                    Select Indicator
-                    <select>
-                        <option value="randomIndicator">{setRandomIndicator}</option>
-                    </select>
+                    Select Indicator:
                 </label>
+                <p></p>
+                <select value={valueInd} onChange={handleChangeInd}>
+                    {randomIndicator.data.map((option) => (
+                        <option key={option.indicator_name} value={option.indicator_name}>
+                            {option.indicator_name}
+                        </option>
+                    ))}
+                </select>
+                <p></p>
                 <label>
-                    Select Country
-                    <select>
-                        <option value="top5Regions">{setCountry}</option>
-                    </select>
+                    Select Country:
                 </label>
-                <h1>Country and Average</h1>
-                <LazyTable route={`http://${config.server_host}:${config.server_port}/top5/:indicator`} columns={top5Regions} />
-                <h1>Country, Indicator, GPI</h1>
-                <LazyTable route={`http://${config.server_host}:${config.server_port}/top5countries/:indicator`} columns={top5Countries} />
+                <p></p>
+                <select value={valueCountries} onChange={handleChangeCountries}>
+                    {countries.data.map((option) => (
+                        <option key={option.countries} value={option.countries}>
+                            {option.countries}
+                        </option>
+                    ))}
+                </select>
+
+                <p>Selected category: {valueCat}</p>
+                <p>Selected indicator: {valueInd}</p>
+                <p>Selected indicator Code: {valueIndCode}</p>
+                <p>Selected Countries: {valueCountries}</p>
+                <p>Encoded indicator: {urlInd}</p>
+
+
+
+
+                <h1>List all countries (in the selected Country’s sub-region) that have average (years 2008-2016) higher values than the selected country’s average for a selected indicator</h1>
+                
+
+                <h1>All indicators from a specified indicator category for all countries where gpi score is less than average (the smaller the score - the more peaceful the country is)</h1>
+                
             </div>
         </Container>
     );
